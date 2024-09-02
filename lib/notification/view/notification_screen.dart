48ac0c47668157d796ebc91a27f74fc443972828
DoYun Joung <rkwhk49@naver.com>
2024-09-02 16:39:56 +0900

    알림 ui 변경 및 삭제 방법 수정

--- lib/notification/view/notification_screen.dart
+++ lib/notification/view/notification_screen.dart
@@ -22,13 +22,17 @@ class NotificationScreen extends ConsumerStatefulWidget {
 }
 
 class _NotificationScreenState extends ConsumerState<NotificationScreen> {
-  // 전체 알림 삭제 기능
-  Future<void> _deleteAllNotifications() async {
+  List<String> selectedNotifications = []; // 선택된 알림 ID 목록
+
+  // 전체 선택된 알림 삭제 기능
+  Future<void> _deleteSelectedNotifications() async {
+    if (selectedNotifications.isEmpty) return;
+
     final confirmDelete = await showDialog<bool>(
       context: context,
       builder: (context) => AlertDialog(
-        title: Text('전체 삭제 확인'),
-        content: Text('모든 알림을 삭제하시겠습니까?'),
+        title: Text('삭제 확인'),
+        content: Text('선택한 알림을 삭제하시겠습니까?'),
         actions: [
           TextButton(
             onPressed: () => Navigator.of(context).pop(false),
@@ -44,11 +48,14 @@ class _NotificationScreenState extends ConsumerState<NotificationScreen> {
 
     if (confirmDelete == true) {
       final repository = ref.read(notificationRepositoryProvider);
-      final notifications = await ref.read(notificationProvider.future);
 
-      for (var notification in notifications) {
-        await repository.deleteNotification(notification.id);
+      for (var id in selectedNotifications) {
+        await repository.deleteNotification(id);
       }
+
+      setState(() {
+        selectedNotifications.clear();
+      });
 
       ref.refresh(notificationProvider);
     }
@@ -89,74 +96,101 @@ class _NotificationScreenState extends ConsumerState<NotificationScreen> {
         ],
         onPopupItemSelected: (value) {
           if (value == 'delete_all') {
-            _deleteAllNotifications();
+            _deleteSelectedNotifications();
           }
         },
         title: '알림',
       ),
       child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: notificationAsyncValue.when(
-            data: (notifications) {
-              if (notifications.isEmpty) {
-                return Center(child: Text('알림이 없습니다.'));
-              }
-              return ListView.separated(
-                separatorBuilder: (context, index) {
-                  return SizedBox(
-                    height: 11,
-                  );
-                },
-                shrinkWrap: true,
-                itemCount: notifications.length,
-                itemBuilder: (context, index) {
-                  final notification = notifications[index];
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: notificationAsyncValue.when(
+                  data: (notifications) {
+                    if (notifications.isEmpty) {
+                      return Center(child: Text('알림이 없습니다.'));
+                    }
+                    return ListView.separated(
+                      separatorBuilder: (context, index) {
+                        return SizedBox(
+                          height: 11,
+                        );
+                      },
+                      shrinkWrap: true,
+                      itemCount: notifications.length,
+                      itemBuilder: (context, index) {
+                        final notification = notifications[index];
+                        final isSelected = selectedNotifications.contains(notification.id);
 
-                  return Dismissible(
-                    key: Key(notification.id), // Dismissible 위젯에 고유 키 제공
-                    direction: DismissDirection.endToStart,
-                    background: Container(
-                      alignment: Alignment.centerRight,
-                      padding: EdgeInsets.only(right: 20),
-                      color: Colors.red,
-                      child: Icon(Icons.delete, color: Colors.white),
-                    ),
-                    onDismissed: (direction) async {
-                      // 개별 알림 삭제 요청
-                      final repository = ref.read(notificationRepositoryProvider);
-                      await repository.deleteNotification(notification.id);
-
-                      // 알림 목록을 다시 불러옵니다.
-                      ref.refresh(notificationProvider);
-                    },
-                    child: notificationBox(
-                      type: notification.type,
-                      title: notification.title,
-                      content: notification.content,
-                    ),
-                  );
-                },
-              );
-            },
-            loading: () => Center(child: CircularProgressIndicator()),
-            error: (error, stackTrace) => Center(child: Text('알림을 불러오는데 실패했습니다.')),
-          ),
+                        return ListTile(
+                          leading: Checkbox(
+                            value: isSelected,
+                            onChanged: (value) {
+                              setState(() {
+                                if (value == true) {
+                                  selectedNotifications.add(notification.id);
+                                } else {
+                                  selectedNotifications.remove(notification.id);
+                                }
+                              });
+                            },
+                          ),
+                          title: notificationBox(
+                            type: notification.type,
+                            title: notification.title,
+                            content: notification.content,
+                            selected: isSelected,
+                          ),
+                          trailing: IconButton(
+                            icon: Icon(Icons.delete, color: Colors.red),
+                            onPressed: () async {
+                              final repository = ref.read(notificationRepositoryProvider);
+                              await repository.deleteNotification(notification.id);
+                              ref.refresh(notificationProvider);
+                            },
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  loading: () => Center(child: CircularProgressIndicator()),
+                  error: (error, stackTrace) => Center(child: Text('알림을 불러오는데 실패했습니다.')),
+                ),
+              ),
+            ),
+            if (selectedNotifications.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: _deleteSelectedNotifications,
+                  child: Text('삭제'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: auctionColor.mainColor,
+                    foregroundColor: Colors.white,
+                    minimumSize: Size.fromHeight(50), // 버튼 높이
+                  ),
+                ),
+              ),
+          ],
         ),
       ),
     );
   }
 
+  // notificationBox 메서드가 클래스 내부에 정의되어야 합니다.
   Container notificationBox({
     required String type,
     required String title,
     required String content,
+    required bool selected,
   }) {
     return Container(
       padding: const EdgeInsets.all(12),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8),
-        color: Colors.white,
+        color: selected ? auctionColor.mainColor.withOpacity(0.1) : Colors.white,
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -206,4 +240,5 @@ class _NotificationScreenState extends ConsumerState<NotificationScreen> {
     );
   }
 }
+
 
